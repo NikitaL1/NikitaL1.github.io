d165379d9c7def3c04188041b7984137c2fb6f63
NikitaL1 <56598090+NikitaL1@users.noreply.github.com>
2020-11-23 00:20:26 +0300

    5-6

---
+++ lab6/script.js
@@ -0,0 +1,14 @@
+function doit()
+{
+    var firstEl = document.getElementById("firstEl").value;
+    var secondEl = document.getElementById("secondEl").value;
+    var thirdEl = document.getElementById("thirdEl").value;
+    console.log(firstEl);
+    var text1 = firstEl + " - very beautiful country";
+    var text2 = secondEl + " - country of modest, intelligent beauty";
+    var text3 = thirdEl + " - a great opportunity to relax with the whole family";
+
+    document.getElementById("result1").innerHTML = text1;
+    document.getElementById("result2").innerHTML = text2;
+    document.getElementById("result3").innerHTML = text3;
+}
